eaaefb3ae7cd87082ad153743eaad5f336c84ab0
Makson <144148309+Sky65678@users.noreply.github.com>
2025-02-18 16:35:54 +0200

    add keys enum in 5ts generic

--- src/generics/5.ts
+++ src/generics/5.ts
@@ -7,7 +7,7 @@ export enum UserRole {
 type UpdateRole = Record<UserRole, string>;
 // Замініть наступний код на версію за допомогою Record
 const RoleDescription: UpdateRole = {
-  admin: "Admin User",
-  editor: "Editor User",
-  guest: "Guest User",
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
 };
